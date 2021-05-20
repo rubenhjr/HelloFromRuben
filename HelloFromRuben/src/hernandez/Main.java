da85fc6c38f13e511dd46febc99c30c9d180c2c9
alouisa <annaastle5@gmail.com>
2021-05-20 16:45:22 -0700

    Hello from Anna

--- HelloFromRuben/src/hernandez/Main.java
+++ HelloFromRuben/src/hernandez/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-            System.out.print("Hello, my name is Ruben.");
+            System.out.println("Hello, my name is Ruben.");
+            System.out.println("Hello Ruben, my name is Anna.");
 
     }
 }
